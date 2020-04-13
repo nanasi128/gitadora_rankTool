f8588778df17fbe36476986f69b8f63f4675ee91
nanasi128 <nanasi_red128@yahoo.co.jp>
2020-04-13 19:08:11 +0900

    Display.html updated

--- HTML/Display.js
+++ HTML/Display.js
@@ -1,20 +1,21 @@
 $(main());
 
+const rank_dic = {
+    "SS" : 0,
+    "S" : 1,
+    "A" : 2,
+    "B" : 3,
+    "C" : 4,
+    "-" : 5
+};
+let songList = [18];
+
 function main(){
     $.ajax({
         url:'https://on50hyixaj.execute-api.ap-northeast-1.amazonaws.com/MyStage',
         success:function(data){
-            var songList = [18];
             var rank = [];
             var classic = [];
-            let rank_dic = {
-                "SS" : 0,
-                "S" : 1,
-                "A" : 2,
-                "B" : 3,
-                "C" : 4,
-                "-" : 5
-            };
             for(var i = 0; i < 18; i++){
                 var o = {
                     "SS" : 0,
@@ -68,11 +69,43 @@ function main(){
                 diff_max -= 0.01;
                 var ap = '<tr align="right"><td>' + diff_min + ' 〜 ' + diff_max + '</td>';
                 Object.keys(val).forEach(function(key){
-                    ap += '<td>' + val[key] + '(' + classic[idx][key] + ')</td>';
+                    ap += '<td onclick="showDetail(' + diff_min + ',' + "'" + key + "'" + ')">' + val[key] + '(' + classic[idx][key] + ')</td>';
                 })
                 ap += '</tr>';
                 $('#summery_table').append(ap);
             })
         }
+    })
+}
+
+function showDetail(_diff, _rank){
+    if($('#detail').find('#detail_table').length != 0){
+        $('#detail_table').remove();
+    }
+    var i = (_diff - 1) / 0.5;
+    var is_matched = false;
+    var matched = [];
+    songList[i].forEach(function(val){
+        if(rank_dic[val.rank] > rank_dic[_rank]){
+            if(!is_matched){
+                $('#detail').append("<table id='detail_table' border='1'><tr><td>#</td><td>曲名</td><td>難易度</td><td>難易度値</td><td>ランク</td></tr></table>");
+                is_matched = true;
+            }
+            var param = {
+                title : val.title,
+                difficulty : val.difficulty,
+                diff_val : val.diff_val,
+                rank : val.rank
+            };
+            matched.push(param);
+        }
+    })
+    if(!is_matched) $('#detail').append('<p>条件にマッチする譜面がありません。</p>');
+    matched.sort(function(a, b){if(a.diff_val > b.diff_val) return 1; if(a.diff_val < b.diff_val) return -1; else return 0;});
+    matched.sort(function(a, b){if(rank_dic[a.rank] > rank_dic[b.rank]) return 1; if(rank_dic[a.rank] < rank_dic[b.rank]) return -1; else return 0;});
+    matched.forEach(function(val, i){
+        var idx = Number(i)+1;
+        var ap = '<tr><td>' + idx + '</td><td>' + val.title + '</td><td>' + val.difficulty + '</td><td>' + val.diff_val +  '</td><td>' + val.rank +  '</td></tr>';
+        $('#detail_table').append(ap);
     })
 }
